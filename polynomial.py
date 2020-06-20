4b880a3bb946f36140e6ff486cedcffb1e5f2b05
Tom Shen <tomshen@berkeley.edu>
2020-06-20 13:32:34 -0700

    implement multiplication, evaluation

--- polynomial.py
+++ polynomial.py
@@ -1,5 +1,5 @@
 import copy
-from typing import Dict
+from typing import Dict, List, Union, Callable
 
 
 class MVLinear:
@@ -38,7 +38,7 @@ class MVLinear:
             i = 0
             while k != 0:
                 if k & 1 == 1:
-                    s += "x"+str(i)
+                    s += "x" + str(i)
                 i += 1
                 k >>= 1
 
@@ -48,13 +48,19 @@ class MVLinear:
     def __copy__(self) -> 'MVLinear':
         t = self.terms.copy()
         return MVLinear(self.num_variables, t, self.p)
+
     __deepcopy__ = __copy__
 
-    def __add__(self, other: 'MVLinear') -> 'MVLinear':
+    def _assert_same_type(self, other: 'MVLinear'):
         if not isinstance(other, MVLinear):
             raise TypeError("MVLinear can only be added with MVLinear")
         if other.p != self.p:
             raise TypeError("The function being added is not in the same field. ")
+
+    def __add__(self, other: Union['MVLinear', int]) -> 'MVLinear':
+        if type(other) is int:
+            other = MVLinear(self.num_variables, {0b0: other}, self.p)
+        self._assert_same_type(other)
 
         ans: MVLinear = copy.copy(self)
         ans.num_variables = max(self.num_variables, other.num_variables)
@@ -69,11 +75,12 @@ class MVLinear:
 
         return ans
 
-    def __sub__(self, other):
-        if not isinstance(other, MVLinear):
-            raise TypeError("MVLinear can only be added with MVLinear")
-        if other.p != self.p:
-            raise TypeError("The function being added is not in the same field. ")
+    __radd__ = __add__
+
+    def __sub__(self, other: Union['MVLinear', int]) -> 'MVLinear':
+        if type(other) is int:
+            other = MVLinear(self.num_variables, {0b0: other}, self.p)
+        self._assert_same_type(other)
 
         ans: MVLinear = copy.copy(self)
         ans.num_variables = max(self.num_variables, other.num_variables)
@@ -87,3 +94,67 @@ class MVLinear:
                 ans.terms[k] = (- other.terms[k]) % self.p
 
         return ans
+
+    def __rsub__(self, other):
+        if type(other) is int:
+            other = MVLinear(self.num_variables, {0b0: other}, self.p)
+        return other - self
+
+    def __mul__(self, other: Union['MVLinear', int]) -> 'MVLinear':
+        if type(other) is int:
+            other = MVLinear(self.num_variables, {0b0: other}, self.p)
+        self._assert_same_type(other)
+
+        terms: Dict[int, int] = dict()
+        # naive n^2 poly multiplication where n is number of terms
+        for sk in self.terms:  # the term of self
+            for ok in other.terms:  # the term of others
+                if sk & ok > 0:
+                    raise ValueError("The product is no longer multi-linear function.")
+                nk = sk + ok  # the result term
+                if nk in terms:
+                    terms[nk] = (terms[nk] + self.terms[sk] * other.terms[ok]) % self.p
+                else:
+                    terms[nk] = (self.terms[sk] * other.terms[ok]) % self.p
+                if terms[nk] == 0:
+                    terms.pop(nk)
+
+        ans = MVLinear(max(self.num_variables, other.num_variables), terms, self.p)
+        return ans
+
+    __rmul__ = __mul__  # commutative
+
+    def eval(self, at: List[int]) -> int:
+        s = 0
+        for term in self.terms:
+            i = 0
+            val = self.terms[term]
+            while term != 0:
+                if term & 1 == 1:
+                    val = (val * (at[i] % self.p)) % self.p
+                if val == 0:
+                    break
+                term >>= 1
+                i += 1
+            s = (s + val) % self.p
+
+        return s
+
+    def __call__(self, *args, **kwargs) -> int:
+        if isinstance(args[0], list):
+            return self.eval(args[0])
+        return self.eval(list(args))
+
+
+def makeMVLinearConstructor(num_variables: int, p: int) -> Callable[[Dict[int, int]], MVLinear]:
+    """
+    Return a function that outputs MVLinear
+    :param num_variables: total number of variables
+    :param p: size of the field
+    :return: Callable[[Dict[int, int]], MVLinear]
+    """
+    def f(term: Dict[int, int]) -> MVLinear:
+        return MVLinear(num_variables, term, p)
+
+    return f
+
