f3c4ab1e7aeb954c01ab8b2b065b782d46c8792b
Tom Shen <tomshen@berkeley.edu>
2020-07-03 14:07:17 -0700

    add completeness test for PMF Interactive Protocol

---
+++ test_IPPMFProver.py
@@ -0,0 +1,19 @@
+import random
+from unittest import TestCase
+from IPPMFProver import InteractivePMFProver, InteractivePMFVerifier
+from PMF import PMF
+from polynomial import randomMVLinear
+
+
+class TestInteractivePMFProver(TestCase):
+    def testCompleteness(self):
+        for _ in range(100):
+            p = PMF([randomMVLinear(6) for _ in range(5)])
+            pv = InteractivePMFProver(p)
+            As, s = pv.calculateAllBookKeepingTables()
+            v = InteractivePMFVerifier(random.randint(0, 0xFFFFFFFFFFFFFFFF), p, s)
+            pv.attemptProve(As, v)
+
+            self.assertTrue(v.convinced)
+
+
