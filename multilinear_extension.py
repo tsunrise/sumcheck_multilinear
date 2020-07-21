ad414aef490f7f6aa79d7664f77d3df73594f7cb
Tom Shen <tomshen@berkeley.edu>
2020-07-21 14:24:48 -0700

    optimize multilinear extension of sparse polynomial

--- multilinear_extension.py
+++ multilinear_extension.py
@@ -1,5 +1,5 @@
 import math
-from typing import List
+from typing import List, Dict
 
 from polynomial import MVLinear, makeMVLinearConstructor
 
@@ -27,6 +27,30 @@ def extend(data: List[int], fieldSize: int) -> MVLinear:
 
     return gen(poly_terms)
 
+
+def extend_sparse(data: Dict[int, int], L: int, fieldSize: int) -> MVLinear:
+    """
+    Convert an sparse map to a polynomial where the argument is the binary form of index.
+    :param data: sparse map if index<2^L. If the size of array is not power of 2, out-of-range part will be arbitrary.
+    :param L: number of variables
+    :param fieldSize: The size of finite field that the array value belongs.
+    :return: The result MVLinear P(x1, x2, ..., xl) = Arr[0bxl...x1]
+    """
+    l: int = L
+    p = fieldSize
+    gen = makeMVLinearConstructor(l, p)
+    x = [gen({1 << i: 1}) for i in range(l)]  # predefine x_1, ..., x_l
+
+    poly_terms = {i: 0 for i in range(2**l)}
+
+    for b, vb in data.items():
+        sub_poly = gen({b: vb})
+        xi0 = [x[i] for i in range(l) if b >> i & 1 == 0]
+        sub_poly *= _product1mx(xi0, 0, len(xi0)-1)
+        for t, v in sub_poly.terms.items():
+            poly_terms[t] += v
+
+    return gen(poly_terms)
 
 def _product1mx(xs: List[MVLinear], lo: int, hi: int):
     """
--- test_multilinear_extension.py
+++ test_multilinear_extension.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from multilinear_extension import extend
+from multilinear_extension import extend, extend_sparse
 import random
 from polynomial import randomPrime
 
@@ -8,9 +8,21 @@ class Test(TestCase):
     def test_extend(self):
         for t in range(10):
             p = randomPrime(64)
-            arr = [random.randint(0, p-1) for _ in range(6000)]
+            arr = [random.randint(0, p-1) for _ in range(1024)]
             poly = extend(arr, p)
             for _ in range(poly.num_variables ** 2):
                 i = random.randint(0, len(arr)-1)
-                self.assertTrue(arr[i], poly.eval_bin(i))
+                self.assertEqual(arr[i], poly.eval_bin(i))
+            print(f"Test #{t} finished. ")
+
+    def test_extend_sparse(self):
+        for t in range(10):
+            p = randomPrime(64)
+            L = 10
+            data = {random.randint(0, (1 << L) - 1): random.randint(0, p-1) for _ in range(256)}
+            poly = extend_sparse(data, L, p)
+            for k in range((1 << L) - 1):
+                expected = data[k] if k in data else 0
+                actual = poly.eval_bin(k)
+                self.assertEqual(expected, actual)
             print(f"Test #{t} finished. ")
