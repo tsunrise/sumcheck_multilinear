7a162245f3b9020e4d9b452bf7aea4cce94147f8
Tom Shen <tomshen@berkeley.edu>
2020-07-22 13:54:53 -0700

    allow direct evaluate multilinear_extension without extending in linear time

--- multilinear_extension.py
+++ multilinear_extension.py
@@ -64,3 +64,54 @@ def _product1mx(xs: List[MVLinear], lo: int, hi: int):
         return L * R
     return 1
 
+
+def evaluate(data: List[int], arguments: List[int],  fieldSize: int) -> int:
+    """
+    Directly evaluate a polynomial based on multilinear extension. The function takes linear time to the size of data.
+    :param data: The bookkeeping table (where the multilinear extension is based on)
+    :param arguments: Input argument
+    :param fieldSize:
+    :return:
+    """
+
+    L = len(arguments)
+    p = fieldSize
+    assert len(data) <= (1 << L), "Insufficient data"
+
+    A: List[int] = data.copy()
+    if len(A) < (1 << L):
+        A += [0] * (1 << L - len(A))
+    for i in range(1, L + 1):
+        r = arguments[i-1]
+        for b in range(2**(L-i)):
+            A[b] = (A[b << 1] * (1 - r) + A[(b << 1) + 1] * r) % p
+    return A[0]
+
+
+def evaluate_sparse(data: Dict[int, int], arguments: List[int], fieldSize: int) -> int:
+    """
+    Sparse version of the function evaluate. The function also takes linear time to the size of data.
+    :param data: dictionary indicating a map between binary argument and its value (sparse bookkeeping table)
+    :param arguments: Input argument
+    :param fieldSize:
+    :return:
+    """
+    L = len(arguments)
+    p = fieldSize
+
+    dp0 = data.copy()
+    dp1: Dict[int, int] = dict()
+    for i in range(L):
+        r = arguments[i]
+        for k, v in dp0.items():
+            if k >> 1 not in dp1:
+                dp1[k >> 1] = 0
+            if k & 1 == 0:
+                dp1[k >> 1] = (dp1[k >> 1] + dp0[k] * (1 - r)) % p
+            else:
+                dp1[k >> 1] = (dp1[k >> 1] + dp0[k] * r) % p
+        dp0 = dp1
+        dp1 = dict()
+    if 0 not in dp0:
+        return 0
+    return dp0[0]
--- test_multilinear_extension.py
+++ test_multilinear_extension.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from multilinear_extension import extend, extend_sparse
+from multilinear_extension import extend, extend_sparse, evaluate, evaluate_sparse
 import random
 from polynomial import randomPrime
 
@@ -26,3 +26,21 @@ class Test(TestCase):
                 actual = poly.eval_bin(k)
                 self.assertEqual(expected, actual)
             print(f"Test #{t} finished. ")
+
+    def test_evaluate(self):
+        for t in range(10):
+            p = randomPrime(64)
+            L = 8
+            arr = [random.randint(0, p-1) for _ in range(1 << L)]
+            poly = extend(arr, p)
+            args = [random.randint(0, p-1) for _ in range(L)]
+            self.assertEqual(poly.eval(args), evaluate(arr, args, p))
+
+    def test_evaluate_sparse(self):
+        for t in range(10):
+            p = randomPrime(64)
+            L = 9
+            data = {random.randint(0, (1 << L) - 1): random.randint(0, p-1) for _ in range(1 << round(L**0.5))}
+            poly = extend_sparse(data, L, p)
+            args = [random.randint(0, p - 1) for _ in range(L)]
+            self.assertEqual(poly.eval(args), evaluate_sparse(data, args, p))
